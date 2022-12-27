9ef6a7a040b3e3ce501c42c7361a1e0acd59a857
Vansh Janmeda <20BCS6263@cuchd.in>
2022-12-27 22:22:28 +0530

    program 1

---
+++ Day 1/Positive/negative_num.cpp
@@ -0,0 +1,22 @@
+/* Positive or Negative number */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter any number: ";
+    int a;
+    cin>>a;
+    if(a>0){
+        cout<<"Positive number";
+    }
+    else if(a==0){
+        cout<<"The number is 0";
+    }
+    else{
+        cout<<"The number is negative";
+    }
+
+    return 0;
+}
